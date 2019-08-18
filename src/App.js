bb64af6b145ab4cecac925150468c907df1a0868
Sunjay <sdsunjay73@yahoo.com>
2019-08-17 19:48:18 -0700

    Passing Method References Between Components

--- src/App.js
+++ src/App.js
@@ -1,30 +1,29 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
-const app = ( props ) => {
-   const [ personsState, setPersonsState ]  = useState({
+class App extends Component {
+  state = {
+    // you can set any properties
     persons: [
       {name: 'Max', age: 28},
       {name: 'Manu', age: 29},
       {name: 'Stephanie', age: 26}
     ],
-    otherState: 'some other value'
-  });
+    otherState: 'some value'
+  }
 
-  //const [otherState, setOtherState] = useState({otherState: 'some other state value'});
-
-  //console.log(personsState, otherState);
   switchNameHandler = (newName) => {
     // console.log('was clicked');
     //DON'T DO THIS: this.state.persons[0].name = 'Maximilian';
+  this.setState(
+
     {persons: [
       {name: newName, age: 30},
       {name: 'Manu', age: 31},
       {name: 'Stephanie', age: 32}
     ]}
   );
-  };
-
+  }
   render() {
     return (
       <div className="App">
@@ -41,8 +40,9 @@ const app = ( props ) => {
         name={this.state.persons[2].name}
         age={this.state.persons[2].age}/>
       </div>
-    );
+      );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
-};
+  }
+}
 
-export default app;
+export default App;
