067abacb7d22cda98129943490d799137f77d988
Sunjay <sdsunjay73@yahoo.com>
2019-08-17 19:35:28 -0700

    Passing Method References Between Components

--- src/App.js
+++ src/App.js
@@ -12,13 +12,13 @@ class App extends Component {
     otherState: 'some value'
   }
 
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     // console.log('was clicked');
     //DON'T DO THIS: this.state.persons[0].name = 'Maximilian';
   this.setState(
 
     {persons: [
-      {name: 'Max', age: 30},
+      {name: newName, age: 30},
       {name: 'Manu', age: 31},
       {name: 'Stephanie', age: 32}
     ]}
@@ -28,10 +28,17 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-      <button onClick={this.switchNameHandler}>Switch Name</button>
-      <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-      <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies: Racing</Person>
-      <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+      <button onClick={this.switchNameHandler.bind(this, 'Maximilian')}>Switch Name</button>
+      <Person
+        name={this.state.persons[0].name}
+        age={this.state.persons[0].age} />
+      <Person
+        name={this.state.persons[1].name}
+        age={this.state.persons[1].age}
+        click={this.switchNameHandler.bind(this, 'Not Maximilian!')}> My hobbies: Racing</Person>
+      <Person
+        name={this.state.persons[2].name}
+        age={this.state.persons[2].age}/>
       </div>
       );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,7 +3,7 @@ import React from 'react';
 const person = ( props ) => {
   return (
   <div>
-    <p>I'm a Person, my name is {props.name} and I'm {props.age} years old!</p>
+    <p onClick={props.click}>I'm a Person, my name is {props.name} and I'm {props.age} years old!</p>
     <p>{props.children}</p>
   </div>
   )
