51f58bfe41c52431fe26a9a91489bb25843e35ec
Sunjay <sdsunjay73@yahoo.com>
2019-08-21 12:57:08 -0700

    Rendering Content Conditionally

--- src/App.js
+++ src/App.js
@@ -16,11 +16,13 @@ class App extends Component {
         {name: 'Stephanie', age: 26}
       ],
       otherState: 'some value',
-
+      showPersons: false
       // Note: think carefully before initializing
       // state based on props!
       // someInitialValue: this.props.initialValue
     }
+    // This binding is necessary to make `this` work in the callback
+    this. togglePersonsHandler = this.togglePersonsHandler.bind(this);
   }
 
   switchNameHandler = (newName) => {
@@ -45,6 +47,12 @@ class App extends Component {
       ]
     } )
   }
+  togglePersonsHandler(){
+
+    this.setState(state => ({
+      showPersons: !state.showPersons
+    }));
+  }
 
   render() {
     const style = {
@@ -55,29 +63,37 @@ class App extends Component {
       cursor: 'pointer'
     };
 
+    let persons = null;
+
+    if(this.state.showPersons ) {
+      persons = (
+            <div>
+              <Person
+                name={this.state.persons[0].name}
+                age={this.state.persons[0].age} />
+              <Person
+                name={this.state.persons[1].name}
+                age={this.state.persons[1].age}
+                click={this.switchNameHandler.bind(this, 'Not Maximilian!')}
+                changed={this.nameChangeHandler}>
+                My hobbies: Racing</Person>
+              <Person
+                name={this.state.persons[2].name}
+                age={this.state.persons[2].age}/>
+            </div>
+      );
+    }
+
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <button
           style={style}
-          onClick={this.switchNameHandler.bind(this, 'Maximilian')}>
-          Switch Name
-        </button>
-      /*we will conditionall show or hide this div*/
-        <div>
-          <Person
-            name={this.state.persons[0].name}
-            age={this.state.persons[0].age} />
-          <Person
-            name={this.state.persons[1].name}
-            age={this.state.persons[1].age}
-            click={this.switchNameHandler.bind(this, 'Not Maximilian!')}
-            changed={this.nameChangeHandler}>
-            My hobbies: Racing</Person>
-          <Person
-            name={this.state.persons[2].name}
-            age={this.state.persons[2].age}/>
-        </div>
+          onClick = { this.togglePersonsHandler }>
+          Toggle Persons
+      </button>
+      {/* We will conditionall show or hide this div */}
+      {persons}
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
