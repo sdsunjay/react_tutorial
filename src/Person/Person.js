6b776f175f899475ec9bca153792a5ee7c90f99f
Sunjay <sdsunjay73@yahoo.com>
2019-08-21 14:36:28 -0700

    Reformatted

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,12 +3,14 @@ import './Person.css';
 
 const person = ( props ) => {
   return (
-  <div className="Person">
-    <p onClick={props.click}>I'm a Person, my name is {props.name} and I'm {props.age} years old!</p>
-    <p>{props.children}</p>
-    <input type="text" onChange={props.changed} value={props.name}/>
-  </div>
-  )
+    <div className="Person">
+      <p onClick={props.click}>
+        I'm a Person, my name is {props.name} and I'm {props.age} years old!
+      </p>
+      <p>{props.children}</p>
+      <input type="text" onChange={props.changed} value={props.name}/>
+    </div>
+  );
 };
 
 export default person;
