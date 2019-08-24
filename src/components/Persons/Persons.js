7f1475de57d2804d4dd6762c8b9ed651a550d61f
Sunjay <sdsunjay73@yahoo.com>
2019-08-23 18:27:01 -0700

    Component Update Lifecycle (for props changes)

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,10 +1,53 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Person from './Person/Person';
 
 class Persons extends Component {
   constructor(props) {
     // Required step: always call the parent class' constructor
     super(props);
+  }
+
+  /*
+  static getDerivedStateFromProps(props, state) {
+    // you don't really use this
+    console.log('[Persons.js] getDerivedStateFromProps');
+    return state;
+  }
+  */
+
+  /*
+  componentWillReceiveProps(props) {
+  // has been removed
+    console.log('[Persons.js] componentWillReceiveProps', props);
+  }
+  */
+
+  shouldComponentUpdate(nextProps, nextState) {
+    // return this.props == nextProps
+    console.log('[Persons.js] shouldComponentUpdate');
+    return true;
+  }
+
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    console.log('[Persons.js] getSnapshotBeforeUpdate');
+    // save some stuff right before the componentDidUpdate
+    return { message: 'Snapshot!' };
+  }
+
+  /**
+  componentWillUpdate(){
+    // has been removed
+    //  should not be used
+  }
+  */
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    // you will use this most often
+    console.log('[Persons.js] componentDidUpdate');
+    console.log(snapshot);
+  }
+  componentDidMount() {
+    console.log('[Persons.js] componentDidMount');
   }
 
   render() {
