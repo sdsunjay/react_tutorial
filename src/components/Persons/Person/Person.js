ed5ae7041a5a806dba35151cb4f0e8c5f80cb727
Sunjay <sdsunjay73@yahoo.com>
2019-08-23 17:56:23 -0700

    Convert from 'functional component' to a 'class component'

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,21 +1,27 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './Person.css';
 
-const person = ( props ) => {
-  /* const rnd = Math.random();
+class Person extends Component {
+  constructor(props) {
+    // Required step: always call the parent class' constructor
+    super(props);
+  }
+  render() {
+    /* const rnd = Math.random();
   if (rnd > 0.1) {
     throw new Error('Something went wrong');
   } */
-  console.log('[Person.js] rendering...');
-  return (
-    <div className="Person" >
-      <p onClick={props.click}>
-        I'm a Person, my name is {props.name} and I'm {props.age} years old!
-      </p>
-      <p>{props.children}</p>
-      <input type="text" onChange={props.changed} value={props.name}/>
-    </div>
-  );
-};
+    console.log('[Person.js] rendering...');
+    return (
+      <div className="Person" >
+        <p onClick={this.props.click}>
+      I'm a Person, my name is {this.props.name} and I'm {this.props.age} years old!
+        </p>
+        <p>{this.props.children}</p>
+        <input type="text" onChange={this.props.changed} value={this.props.name}/>
+      </div>
+    );
+  }
+}
 
-export default person;
+export default Person;
--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,19 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Person from './Person/Person';
 
-const persons = (props) => {
-  console.log('[Persons.js] rendering...');
-  return props.persons.map( (person, index) => {
-    return (
-      <Person
-        click={() => props.clicked( index )}
-        name={person.name}
-        age={person.age}
-        key={person.id}
-        changed={( event ) => props.changed(event, person.id)}
-      />
-    );
-  });
-};
+class Persons extends Component {
+  constructor(props) {
+    // Required step: always call the parent class' constructor
+    super(props);
+  }
 
-export default persons;
+  render() {
+    console.log('[Persons.js] rendering...');
+    return this.props.persons.map( (person, index) => {
+      return (
+        <Person
+          click={() => this.props.clicked( index )}
+          name={person.name}
+          age={person.age}
+          key={person.id}
+          changed={( event ) => this.props.changed(event, person.id)}
+        />
+      );
+    });
+  };
+}
+export default Persons;
