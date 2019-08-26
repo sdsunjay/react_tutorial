8a2f88e55c581b64e8448843153dfbd9e6883b2b
Sunjay <sdsunjay73@yahoo.com>
2019-08-25 18:37:21 -0700

    formatting

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -72,10 +72,10 @@ class Persons extends PureComponent {
     return this.props.persons.map((person, index) => {
       return (
         <Person click = {() => this.props.clicked(index)}
-        name = {person.name}
-        age = {person.age}
-        key = {person.id}
-        changed = {(event) => this.props.changed(event, person.id)}
+          name = {person.name}
+          age = {person.age}
+          key = {person.id}
+          changed = {(event) => this.props.changed(event, person.id)}
         />
       );
     });
